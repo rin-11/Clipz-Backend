f179efa907bb6128a23577b19a9503a7193c23a9
Rin Kates <erinkates11@gmail.com>
2023-05-25 16:51:48 -0400

    mongoose connected

--- server.js
+++ server.js
@@ -1,20 +1,34 @@
 
 // DEPENDENCIES
-require("dotenv").config();
-
-// pulls port from the .env file
-const { PORT } = process.env;
-// import express
 const express = require("express");
-// create application object
 const app = express();
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+
+
+// .env dependency PORT
+require('dotenv').config();
+
+// database connection
+mongoose.connect(process.env.DATABASE_URL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+}); //gets rid of deprecation warnings
+
+
+    // Mongo error/success messages
+const db = mongoose.connection
+db.on('error', (err) => console.log(`${err.message} MongoDB Not Running!`));
+db.on('connected', () => console.log('mongo connected'));    
+db.on('disconnected', () => console.log('mongo disconnected'));
 
 
 // Routes 
-app.get("/", (req, res) => {
-  res.send("app is working");
-});
 
 
+// PORT
+const PORT = process.env.PORT || 4000;
 // Listener
-app.listen(PORT, () => console.log(`listening on PORT ${PORT}`));
+app.listen(PORT, ()=>{
+  console.log(`Server is listening on port ${PORT}`);
+});
