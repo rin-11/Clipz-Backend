cd94716f4f3cd21b55f5c0ef10bbd4a08386483b
Rin Kates <erinkates11@gmail.com>
2023-05-25 22:48:10 -0400

    get user working

--- controllers/userCtrl.js
+++ controllers/userCtrl.js
@@ -1,18 +1,23 @@
-// USER CRUD CONTROLLER
-
-    // Get a User
-
-    // Get all users
-
-    // Update a user
-
-    // Delete a user
-
-    // Follow a User
-
-    // Unfollow a User
+const UserModel = require('../models/userModel')
 
 
-
+// Get a User
+exports.getUser = async (req, res) => {
+    // extract the id parame from the request params (req.params) & return userID
+    const id = req.params.id;
+  
+    try {
+      const user = await UserModel.findById(id);
+      if (user) { // if user is found by ID return user information
+        const { password, ...otherDetails } = user._doc;
+            // extracts the password from the user info before returning
+        res.status(200).json(otherDetails); // user details excluding the password
+      } else {
+        res.status(404).json("User Not Found");
+      }
+    } catch (error) {
+      res.status(500).json(error);
+    }
+  };
 
 
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,11 +1,24 @@
 const express = require('express');
-
 const router = express.Router();
+const { getUser } = require('../controllers/userCtrl')
 
+// ** just use for route testing **
+// router.get('/', async(req, res)=>{res.send("Test User Route")}) 
 
+// Get User by ID
+router.get('/:id', getUser);
 
-router.get('/', async(req, res)=>{res.send("Test User Route")}) 
+// Get all users
 
+// Update a user
+
+// Delete a user
+
+// Follow a User
+
+// Unfollow a User
 
 
 module.exports = router;
+
+
