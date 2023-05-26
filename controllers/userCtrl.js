1e2f15861239e540b54c095a8da9762414a5e195
Rin Kates <erinkates11@gmail.com>
2023-05-25 21:09:39 -0400

    bcrypt working for user register

--- controllers/userCtrl.js
+++ controllers/userCtrl.js
@@ -1,15 +1,34 @@
 const UserModel = require('../models/userModel')
+const bcrypt = require("bcrypt");
 
+// Export registerUser function as a named export
 exports.registerUser = async (req, res) => {
+
     const { email, password, displayname } = req.body;
-  
-    const newUser = new UserModel({ email, password, displayname });
-  
+      // destructe & extract the email, password, &displayname properties from the req.body object 
+      // assign the extracted values to corresponding variables
+
+      // BCRYPT
+        // generate a salt from bcrypt library
+      const salt = await bcrypt.genSalt(10)
+        // create variable for the hashed password 
+      const hash = await bcrypt.hash(password, salt)
+
+    // create a new instance of the UserModel using the extracted values 
+    const newUser = new UserModel({ 
+          email, 
+          password:hash, // give password the value of hash
+          displayname
+        });
+    
+  // start a try block to handle potential errors 
     try {
+      // save the user data to the database
       await newUser.save();
-      res.status(200).json(newUser); // success status code
-    } catch (error) {
-      res.status(500).json({ message: error.message }); // error status code
+      // send success status code & newUser object in JSON format when registration is successful
+      res.status(200).json(newUser); 
+    } catch (error) { // if an error occurred during the registration process
+      res.status(500).json({ message: error.message }); // send error status code
     }
   };
 
